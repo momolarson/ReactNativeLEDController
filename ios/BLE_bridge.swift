807a29cb52c56d94b8d7f398d960d29e5a4aa262
Gregg Larson <larsongb@gmail.com>
2020-06-24 09:01:01 -0600

    yarn security update

---
+++ ios/BLE_bridge.swift
@@ -0,0 +1,9 @@
+//
+//  BLE_bridge.swift
+//  BluetoothExample
+//
+//  Created by Gregg Larson on 6/24/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+import Foundation
---
+++ ios/File.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  BluetoothExample
+//
+//  Created by Gregg Larson on 6/24/20.
+//  Copyright © 2020 Facebook. All rights reserved.
+//
+
+import Foundation
